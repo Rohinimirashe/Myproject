604189eafa6aa0f3d77e2fc01a3a07b100de25aa
Neeraj <neerajap.01@gmail.com>
2022-04-27 13:07:49 +0530

    Blog Project-1 Phase-I completed

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,5 +6,9 @@ const router = express.Router();
 
 router.post('/authors', Author.addAuthor);
 router.post('/blog', Blog.createBlog);
+router.get('/blogs', Blog.getBlogs);
+router.put('/blog/:blogId', Blog.updateBlog);
+router.delete('/blog/:blogId', Blog.deleteBlogById);
+router.delete('/blogs', Blog.deleteBlogs);
 
 module.exports = router;
