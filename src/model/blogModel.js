ff79d86bfb10d0de1f06086d71005d7863ef5e69
Neeraj <neerajap.01@gmail.com>
2022-04-27 21:08:42 +0530

    Removed Unwanted Import

--- src/model/blogModel.js
+++ src/model/blogModel.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const { required } = require('nodemon/lib/config');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const blogSchema = new mongoose.Schema({
